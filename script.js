3d35978f5fd7f621a1f8f4744dc1ee0508b473b2
qelsimrraci <qelsim2005@gmail.com>
2025-06-02 12:58:21 +0100

    commit

--- script.js
+++ script.js
@@ -46,3 +46,8 @@ window.addEventListener('scroll', () => {
 });
 
     
+const toggleBtn = document.getElementById("toggle-btn");
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+});
